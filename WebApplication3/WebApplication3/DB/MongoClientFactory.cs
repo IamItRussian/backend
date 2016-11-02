2c3206eeef5b39d29ff9dfc9475d645663e2e0a7
artembell <artembell305@gmail.com>
2016-11-02 18:36:24 +0300

    Second commit

---
+++ WebApplication3/WebApplication3/DB/MongoClientFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+
+namespace WebApplication3.DB
+{
+    public class MongoClientFactory
+    {
+        public static MongoClient GetMongoClient()
+        {
+            var dbConnectionString = System.Configuration.ConfigurationManager.AppSettings["DB"];
+            if (!String.IsNullOrEmpty(dbConnectionString))
+            {
+                return new MongoClient(dbConnectionString);
+            }
+            return new MongoClient();
+        }
+        public static IMongoDatabase GetMongoDatabase(String name = "mongodb")
+        {
+            return GetMongoClient().GetDatabase(name);
+        }
+    }
+}
